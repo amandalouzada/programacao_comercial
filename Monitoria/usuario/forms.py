8a48154b364934fb224442212d65634d885c79a2
Amanda Louzada <amandanuneslouzada@gmail.com>
2017-02-08 20:05:41 -0300

    Delete

--- Monitoria/usuario/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from usuario.models import *
-
-class FormularioUsuario(forms.ModelForm):
-
-    class Meta:
-        model = Usuario
-        exclude = []
--- README.md
+++
@@ -1,4 +0,0 @@
-#Disciplina de programação comercial
-  Projeto desenvolvido na disciplina de programação comercial
-##Louzada
-    O que eu faço durante a aula
